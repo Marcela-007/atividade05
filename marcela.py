04c7aec69368c1ca49f9300414aa1659c28e1191
Marcela-007 <marcelaoliveira446677@gmail.com>
2023-10-11 11:36:28 -0300

    atividade05-centimetros e milimetros

---
+++ marcela.py
@@ -0,0 +1,5 @@
+metragem = float(input("Insira a sua metragem "))
+cm = metragem * 100
+mm = metragem * 1000
+print("Sua metragem em centimetros é " , cm)
+print("Sua metragem em milimetros é " , mm)
